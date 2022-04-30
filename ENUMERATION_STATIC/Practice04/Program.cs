91e243338edbbf9629c4ec0ded3737e97d436a0a
DESKTOP-UI0F5QE\ADMIN <tntao.khtn@gmail.com>
2022-05-01 01:46:23 +0700

    Practice04

---
+++ ENUMERATION_STATIC/Practice04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace Practice04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            DOWConverter dOWConverter = new DOWConverter();
+            Console.WriteLine(dOWConverter.ConvertDaytoWeek(8, 5, 2022));
+
+            Console.ReadLine();
+        }
+    }
+}
